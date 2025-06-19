e41e3c3a2903d5e071a801f815b406b739bc24b4
CodeUpSubodh <subodhsrivastava03@gmail.com>
2025-06-20 00:02:22 +0530

    Add create user

--- routes/routes.go
+++ routes/routes.go
@@ -1,11 +1,14 @@
 package routes
 
 import (
-    "github.com/gorilla/mux"
-    "gorm.io/gorm"
-    "your-go-app/views"
+	"your-go-app/views"
+
+	"github.com/gorilla/mux"
+	"gorm.io/gorm"
 )
 
 func RegisterRoutes(r *mux.Router, db *gorm.DB) {
-    r.HandleFunc("/users", views.GetUsers(db)).Methods("GET")
+	r.HandleFunc("/users", views.GetUsers(db)).Methods("GET")
+	r.HandleFunc("/user", views.GetUsers(db)).Methods("POST")
+
 }
--- views/user_view.go
+++ views/user_view.go
@@ -1,17 +1,45 @@
 package views
 
 import (
-    "encoding/json"
-    "net/http"
-    "your-go-app/models"
+	"encoding/json"
+	"net/http"
+	"your-go-app/models"
 
-    "gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 func GetUsers(db *gorm.DB) http.HandlerFunc {
-    return func(w http.ResponseWriter, r *http.Request) {
-        var users []models.User
-        db.Find(&users)
-        json.NewEncoder(w).Encode(users)
-    }
+	return func(w http.ResponseWriter, r *http.Request) {
+		var users []models.User
+		db.Find(&users)
+		json.NewEncoder(w).Encode(users)
+	}
+}
+
+func CreateUser(db *gorm.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var user models.User
+
+		// Decode the request body into user struct
+		if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+			http.Error(w, "Invalid input: "+err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		// Basic validation (you can enhance this)
+		if user.Name == "" || user.Email == "" {
+			http.Error(w, "Name and Email are required fields", http.StatusBadRequest)
+			return
+		}
+
+		// Create user in the database
+		if err := db.Create(&user).Error; err != nil {
+			http.Error(w, "Could not create user: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		// Respond with the created user
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(user)
+	}
 }
